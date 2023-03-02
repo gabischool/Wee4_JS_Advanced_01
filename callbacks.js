93f8ef71abf4de27a9b1a115e1080602ab4723a5
ayan jama <ayaanjaamac1999@gmail.com>
2023-03-02 22:45:41 +0300

    finish

--- callbacks.js
+++ callbacks.js
@@ -16,30 +16,79 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   // Function invocation 
   firstItem(items, function(first) {
     console.log(first)
-  });
+  });           
 */
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+return cb(arr) 
 }
+
+getLength(items, (lengths) =>{
+console.log(lengths);
+})
+
+
+
+
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[3])
 }
+
+last(items, (lastitem) =>{
+  console.log(lastitem);
+})
+
+
+
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y)
 }
+
+const add = (x, y) =>{
+  return x + y;
+}
+
+console.log(sumNums(3, 4, add))
+
+
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y)
 }
+
+const multiplies = (x, y) =>{
+  return x * y;
+}
+
+ console.log(multiplyNums(5, 3,mu))
+
+
+
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list)
 }
+
+// contains(items, (lists) =>{
+//   console.log(lists)
+// })
+
+
+
 
 /* STRETCH PROBLEM */
 
