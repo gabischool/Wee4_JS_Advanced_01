6a89debfc59ac20cfb6a1b3f651b3062f2269b30
M-Maxamed <m.maxameadow@gmail.com>
2023-03-03 09:41:38 +0100

    waan Kashaqeyay cashirka

--- callbacks.js
+++ callbacks.js
@@ -2,7 +2,7 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+
   //Given this problem: 
   
   function firstItem(arr, cb) {
@@ -17,28 +17,33 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   firstItem(items, function(first) {
     console.log(first)
   });
-*/
+
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(4 + 6);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(4 * 3);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
 
 /* STRETCH PROBLEM */
@@ -47,4 +52,11 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+    const result = [];
+    array.forEach(item => {
+      if (!result.includes(item)) {
+        result.push(item);
+      }
+    });
+    cb(result);
 }
