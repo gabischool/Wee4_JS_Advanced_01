e8a28c261ca3002e0e529964b96e69326070e8a4
Bilal-Ibrahim1 <bilalohared@gmail.com>
2025-01-11 23:55:31 +0100

    I have finished the tasks

--- challenges.js
+++ challenges.js
@@ -1,5 +1,5 @@
-/*
-Task 1: Library Late Fee Calculator 🚀🚀🚀🚀
+
+/*Task 1: Library Late Fee Calculator 🚀🚀🚀🚀
 
 You are working at a library and need to calculate late 
 fees for overdue books. Write a function called `calculateLateFee` 
@@ -7,28 +7,40 @@ that takes the number of overdue days as input and calculates
 the fee ($0.25 per day). Prompt the user to input the
 number of overdue days.
 
-Log the result as:
+*///get the result as:
 
-"The late fee is $[amount]."
+//"The late fee is $[amount]."
 
-Example:
-User Input: 10
-Output: "The late fee is $2.50."
-*/
-
-// ✍️ ✍️ ✍️ ✍️ Write the function here ✍️ ✍️ ✍️ ✍️
+//Example:
+//User Input: 10
+//Output: "The late fee is $2.50."
 
 
+
+//✍️ Write the function here ✍️ ✍️ ✍️ ✍️
+
+function calculateLateF(){
+  let days = prompt("please enter your overdue days:");
+  let fee = days * 0.25; 
+  console.log("The late fee is $" + fee); 
+}
+calculateLateF();
 
 // Extra Task:
 // - Convert the function into a function expression.
 
+const calculateLateFee = function(){
+  let days = prompt("please enter your overdue days:");
+  let fee = days * 0.25; 
+  console.log("The late fee is $" + fee); 
+}
+calculateLateFee();
 
 
-/*
-Task 2 : Favorite Color Finder 🚀🚀🚀🚀
 
-You are designing a game that asks players for their favorite color. 
+//Task 2 : Favorite Color Finder 🚀🚀🚀🚀
+
+/*You are designing a game that asks players for their favorite color. 
 Write a function called `findColorMeaning` that takes a color as input 
 (prompt the user) and logs the following meanings:
 
@@ -44,18 +56,46 @@ Output: "Red: You are passionate and bold."
 */
 
 // ✍️ ✍️ ✍️ ✍️ Write the function here ✍️ ✍️ ✍️ ✍️
+let color = prompt("Please write down here your favorite color")
+
+function findColorMeaning(color) {
+ if (color=="blue"){console.log("You love calm and peace");} 
+
+    else if(color=="Red"){console.log("You are passionate and bold.");}
+
+      else if (color=="Gree") { console.log("you are connected to nature.");}
+
+     else if (color== "Yellow"){ console.log("You radiate happiness and energy.");} 
+ else{ console.log("That's a unique choice!");}                                                          
+}
+
+findColorMeaning(color)
+  
 
 
 
-// Extra Task:
+ //Extra Task:
 // - Rewrite the function using an arrow function.
 
+const findColorMean=(color)=>{
+  let colorName= prompt("Please write down here your favorite color")
 
+ if (color=="blue"){console.log("You love calm and peace");} 
 
-/*
-Task 3 : Lawyer's Case Log 🚀🚀🚀🚀
+    else if(color=="Red"){console.log("You are passionate and bold.");}
 
-You’re a lawyer, and you’ve been assigned to organize your case files. 
+      else if (color=="Gree") { console.log("you are connected to nature.");}
+
+     else if (color== "Yellow"){ console.log("You radiate happiness and energy.");} 
+ else{ console.log("That's a unique choice!");}                                                          
+}
+findColorMean("color");
+                          
+ 
+
+//Task 3 : Lawyer's Case Log 🚀🚀🚀🚀
+
+/*You’re a lawyer, and you’ve been assigned to organize your case files. 
 Write a function called `logCase` that takes the client's name and 
 the case number as parameters. For each case, log:
 
@@ -68,14 +108,24 @@ Output: "Case #12345: John Doe's case is now logged."
 
 // ✍️ ✍️ ✍️ ✍️ Write the function here ✍️ ✍️ ✍️ ✍️
 
+function logCase(name,caseNumber){
 
+  console.log("Case # 12345: John Doe's case is now logged.");
+}
+logCase("John Doe"+12345)
 
-// Extra Task:
-// - Rewrite the function as an arrow function.
+ //Extra Task:
+ //- Rewrite the function as an arrow function.
 
+const logcase=(name,caseNumber) =>{
 
-/*
-Task 4 : Attendance Tracker 🚀🚀🚀🚀
+  console.log("Case #12345: John Doe's case is now logged.");
+}
+logcase("John Doe"+12345)
+
+ 
+
+/*Task 4 : Attendance Tracker 🚀🚀🚀🚀
 
 You’re a teacher tracking student attendance. Write a function called 
 `markAttendance` that takes a student’s name and a boolean `isPresent` 
@@ -94,10 +144,41 @@ Output: "Amina is present."
 
 // ✍️ ✍️ ✍️ ✍️ Write the function here ✍️ ✍️ ✍️ ✍️
 
+function markAttendance(studentName, isPresent ){
+
+  if(isPresent) {
+
+    console.log(studentName, "is present")
+
+  } else { 
+
+    console.log(studentName, "is absent")
+ }
+
+}
+
+markAttendance("Amina", true);
 
 
 // Extra Task:
 // - Convert the function into a function expression.
+const studentAttendance = function (studentName, isPresent ){
+
+  if(isPresent) {
+
+    console.log(studentName, "is present")
+
+  } else { 
+
+    console.log(studentName, "is absent")
+ }
+
+}
+
+studentAttendance("Amina", true);
+
+
+
 
 
 
@@ -122,16 +203,4 @@ You are a teacher, and you want to automate the creation of detailed grade repor
      "[StudentName] - Average Score: [average], Grade: [letterGrade]"
 
 4. Finally, use the `prompt` function to input the student’s name and three test scores, and display the generated report using `console.log`.
-
-Example:
-User Input:
-Name: Amina
-Scores: 85, 90, 78
-
-Output:
-"Amina - Average Score: 84.33, Grade: B"
-
-Extra Credit:
-- Extend the program to accept multiple students' names and scores and generate a report for each student using a loop.
-- Use an arrow function for at least one of the functions.
 */
